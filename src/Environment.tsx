2ad8d1109c9b318191b1e4e983da7e9eb8ffea90
Dan Wang <github@danwanf.com>
2018-03-22 12:24:59 -0700

    Remove Static

--- src/Environment.tsx
+++ src/Environment.tsx
@@ -1,13 +1,50 @@
 import * as React from "react";
-import { Static } from "./Static";
-import { RenderProp } from "./RenderProp";
 
-export interface EnvironmentI<T> {
-  map: <U extends any>(mapper: (t: T) => U) => Environment<U>;
-  flatMap: <U extends any>(mapper: (t: T) => Environment<U>) => Environment<U>;
-  toReactElement: <U extends React.ReactElement<any>>(
+type ConsumerProps<T> = {
+  children: (value: T) => React.ReactNode;
+};
+export type Consumer<T> = React.ComponentType<ConsumerProps<T>>;
+
+export class Environment<T> {
+  readonly ConsumerComponent: Consumer<T>;
+
+  constructor(ConsumerComponent: Consumer<T>) {
+    this.ConsumerComponent = ConsumerComponent;
+  }
+
+  map = <U extends any>(mapper: (t: T) => U): Environment<U> => {
+    const { ConsumerComponent } = this;
+    return make<U>((props: ConsumerProps<U>) => (
+      <ConsumerComponent>
+        {(t: T) => props.children(mapper(t))}
+      </ConsumerComponent>
+    ));
+  };
+
+  flatMap = <U extends any>(
+    mapper: (t: T) => Environment<U>
+  ): Environment<U> => {
+    const Outer = this.ConsumerComponent;
+    return make<U>((props: ConsumerProps<U>) => (
+      <Outer>
+        {(t: T) => {
+          const env = mapper(t);
+          const Inner = env.ConsumerComponent;
+          return <Inner>{props.children}</Inner>;
+        }}
+      </Outer>
+    ));
+  };
+
+  toReactElement = <U extends React.ReactElement<any>>(
     this: Environment<U>
-  ) => React.ReactElement<any>;
+  ): React.ReactElement<any> => {
+    const { ConsumerComponent } = this;
+    return <ConsumerComponent>{x => x}</ConsumerComponent>;
+  };
 }
 
-export type Environment<T> = RenderProp<T> | Static<T>;
+const make = <T extends any>(consumer: Consumer<T>): Environment<T> =>
+  new Environment(consumer);
+
+export default { make };
